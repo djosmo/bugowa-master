0730b1fc18b0d212f9d8d7dd895e8a9fcfb458eb
djosmo <krzysztof.czjk@gmail.com>
2020-07-09 23:47:56 +0200

    zmiana argumentów detail

--- blog/models.py
+++ blog/models.py
@@ -38,8 +38,6 @@ class Post(models.Model):
     def __float__(self):
         return self.title
 
-    def get_absolute_url(self):
-        return reverse('blog:post_detail',
-                       args=[self.post_id])
+)
 
 
